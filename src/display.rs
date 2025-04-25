0bfedcc0cbcb073f66ce63a39728f01eba8c6c6a
Nowu <nowu.user@gmail.com>
2025-04-25 18:56:45 -0500

    change display expr symbols

--- src/display.rs
+++ src/display.rs
@@ -9,8 +9,8 @@ impl Display for Expr {
             match self {
                 Expr::Var(p) => p.to_string(),
                 Expr::Not(p) => format!("~ {p}"),
-                Expr::Or(p, q) => format!("({p} or {q})"),
-                Expr::And(p, q) => format!("({p} and {q})"),
+                Expr::Or(p, q) => format!("({p} ∨ {q})"),
+                Expr::And(p, q) => format!("({p} ∧ {q})"),
                 Expr::Conditional(p, q) => format!("({p} -> {q})"),
                 Expr::BiConditional(p, q) => format!("({p} <-> {q})"),
             }
