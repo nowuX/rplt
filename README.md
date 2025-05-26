00808a252ae83139cecfa49ebb0b9189c842e2cb
Nowu <nowu.user@gmail.com>
2025-05-25 19:06:56 -0500

    Adding Clap argument parser

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# RLPT
+# RPLT
 RPLT (Rust Propositional Logic Table) is a lightweight truth table generator for propositional logic expressions. Written in Rust, it parses logical formulas and outputs their complete truth tables. Supports operators like NOT, AND, OR, CONDITIONAL and BICONDITIONAL
 
 # Build From Source
@@ -10,30 +10,23 @@ Install [Rust](https://www.rust-lang.org/tools/install).
 ### Building the Project
 1. Clone this repository to your local machine:
 ```bash
-git clone https://github.com/nowuX/rlpt.git
-cd rlpt
-cargo run
+$ git clone https://github.com/nowuX/rplt.git
+$ cd rplt
+$ cargo build
 ```
 
 ## Usage
-Change the `s` value in `main.rs`
-```rust
-// in main.rs
-fn main() {
-    let s = vec![
-        "p -> ( p or q )",
-        "( p and q ) and ~ p",
-        // Add or edit this
-    ];
-}
-// cargo run # in console
+```bash
+$ cargo run -- table "( p and q ) -> q"
+$ cargo run -- --verbose table "( q or p ) -> r"
+$ cargo run -- help
 ```
 ### Output
 ![A](assets/table.png)
 
 ## TODO
-- [ ] Support arguments with [clap](https://lib.rs/crates/clap)
-  - [ ] Only vars values and final result
+- [X] Support arguments with [clap](https://lib.rs/crates/clap)
+  - [X] Only vars values and final result
   - [ ] Tautology, contradiction and contingency indicator (IDK)
 - [X] Verbose in each operation. `p or q` -> `V [V] F`
 - [X] Better display operation `~ p` to `<color>~ p`
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,46 @@
+use clap::{Arg, ArgAction, Command};
 use rplt::eval::eval;
 use rplt::expr_parser::tokens_to_expr;
 use rplt::table::generate_table;
 use rplt::token_parser::{parser, vars_values};
 
 fn main() {
-    let s = vec![
-        "p -> ( p or q )",
-        "( p and q ) and ~ p",
-    ];
-    for i in s {
-        let mut tokens = parser(i);
-        let mut ctx = vars_values(&tokens);
-        let expr = tokens_to_expr(&mut tokens);
-        let _ = eval(&expr, &mut ctx);
-        println!("{}\n", generate_table(&ctx));
+    let args = Command::new("rplt")
+        .version("1.0.0")
+        .author("Nowu <nowu.user@gmail.com>").about("RPLT (Rust Propositional Logic Table) is a truth table generator for propositional logic expressions. Written in Rust, it parses logical formulas and outputs their complete truth tables. Supports operators like NOT, AND, OR, CONDITIONAL and BICONDITIONAL")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .arg(
+            Arg::new("verbose")
+                .short('v')
+                .long("verbose")
+                .help("Enable verbose operation")
+                .action(ArgAction::SetTrue)
+        )
+        .subcommand(
+            Command::new("table")
+                .about("Display a logic propositional table from the input")
+                .arg(
+                    Arg::new("input")
+                        .help("Input string for the table")
+                        .required(true)
+                        .index(1)
+                )
+        );
+    let matches = args.get_matches();
+    let verbose = matches.get_flag("verbose");
+
+    match matches.subcommand() {
+        Some(("table", a)) => {
+            let i = a.get_one::<String>("input").unwrap();
+            let mut tokens = parser(&i);
+            let mut ctx = vars_values(&tokens);
+            let expr = tokens_to_expr(&mut tokens);
+            let _ = eval(&expr, &mut ctx);
+            println!("{}\n", generate_table(&ctx, verbose));
+        }
+        _ => {
+            eprintln!("Unable to process sub command");
+        }
     }
 }
