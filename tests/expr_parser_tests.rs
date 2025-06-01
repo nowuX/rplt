7ede5b51aad3b20edf522bfa4959930b83e395ea
Nowu <nowu.user@gmail.com>
2025-06-01 14:02:37 -0500

    `test_case` hit

--- tests/expr_parser_tests.rs
+++ tests/expr_parser_tests.rs
@@ -1,5 +1,6 @@
 use rplt::expr_parser::{exec_pattern, expr_matcher, tokens_to_expr};
 use rplt::{Expr, Op, Token};
+use test_case::test_case;
 use std::sync::Arc;
 
 #[test]
@@ -31,66 +32,32 @@ fn exec_pattern_test() {
     );
 }
 
-#[test]
-fn expr_matcher_test() {
-    for i in [
-        Token::OpenParen,
-        Token::CloseParen,
-        Token::Op(Op::Not),
-        Token::Var(String::from("r")),
-    ] {
-        assert_eq!(
-            expr_matcher(
-                &i,
-                Box::new(Expr::Var(String::from("p"))),
-                Box::new(Expr::Var(String::from("q")))
-            ),
-            None,
-        );
-    }
+#[test_case(Token::OpenParen; "open_paren")]
+#[test_case(Token::CloseParen; "close_paren")]
+#[test_case(Token::Op(Op::Not); "not_op")]
+#[test_case(Token::Var(String::from("r")); "var_r")]
+fn expr_matcher_returns_none(token: Token) {
+    assert_eq!(
+        expr_matcher(
+            &token,
+            Box::new(Expr::Var(String::from("p"))),
+            Box::new(Expr::Var(String::from("q")))
+        ),
+        None,
+    );
+}
 
-    assert_eq!(
-        expr_matcher(
-            &Token::Op(Op::Or),
-            Box::new(Expr::Var(String::from("p"))),
-            Box::new(Expr::Var(String::from("q")))
-        ),
-        Some(Expr::Or(
-            Box::new(Expr::Var(String::from("p"))),
-            Box::new(Expr::Var(String::from("q")))
-        )),
-    );
-    assert_eq!(
-        expr_matcher(
-            &Token::Op(Op::And),
-            Box::new(Expr::Var(String::from("p"))),
-            Box::new(Expr::Var(String::from("q")))
-        ),
-        Some(Expr::And(
-            Box::new(Expr::Var(String::from("p"))),
-            Box::new(Expr::Var(String::from("q")))
-        )),
-    );
-    assert_eq!(
-        expr_matcher(
-            &Token::Op(Op::Conditional),
-            Box::new(Expr::Var(String::from("p"))),
-            Box::new(Expr::Var(String::from("q")))
-        ),
-        Some(Expr::Conditional(
-            Box::new(Expr::Var(String::from("p"))),
-            Box::new(Expr::Var(String::from("q")))
-        )),
-    );
-    assert_eq!(
-        expr_matcher(
-            &Token::Op(Op::BiConditional),
-            Box::new(Expr::Var(String::from("p"))),
-            Box::new(Expr::Var(String::from("q")))
-        ),
-        Some(Expr::BiConditional(
-            Box::new(Expr::Var(String::from("p"))),
-            Box::new(Expr::Var(String::from("q")))
-        )),
-    );
+#[test_case(Token::Op(Op::Or), Expr::Or; "or_op")]
+#[test_case(Token::Op(Op::And), Expr::And; "and_op")]
+#[test_case(Token::Op(Op::Conditional), Expr::Conditional; "conditional_op")]
+#[test_case(Token::Op(Op::BiConditional), Expr::BiConditional; "biconditional_op")]
+fn expr_matcher_creates_expr(token: Token, expected_variant: fn(Box<Expr>, Box<Expr>) -> Expr) {
+    let p = Box::new(Expr::Var(String::from("p")));
+    let q = Box::new(Expr::Var(String::from("q")));
+    let expected = Some(expected_variant(
+        Box::new(Expr::Var(String::from("p"))),
+        Box::new(Expr::Var(String::from("q")))
+    ));
+
+    assert_eq!(expr_matcher(&token, p, q), expected);
 }
